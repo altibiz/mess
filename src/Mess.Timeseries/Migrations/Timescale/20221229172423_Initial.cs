85b826d3cabc714eb7b734507d609e8c412fb5e5
Hrle <32d9c8c6-8118-45e7-857e-8b45522f395d@anonaddy.me>
2022-12-29 18:24:37 +0100

    WIP

---
+++ src/Mess.Timeseries/Migrations/Timescale/20221229172423_Initial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mess.Migrations.Timescale
+{
+  public partial class Initial : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.CreateTable(
+        name: "Measurements",
+        columns: table =>
+          new
+          {
+            Timestamp = table.Column<DateTime>(
+              type: "timestamp with time zone",
+              nullable: false
+            ),
+            SourceId = table.Column<string>(type: "string", nullable: false),
+            Power = table.Column<float>(type: "float", nullable: false),
+            Voltage = table.Column<float>(type: "float", nullable: false),
+            Tenant = table.Column<string>(type: "string", nullable: false)
+          },
+        constraints: table =>
+        {
+          table.PrimaryKey(
+            "PK_Measurements",
+            x => new { x.Timestamp, x.SourceId }
+          );
+        }
+      );
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropTable(name: "Measurements");
+    }
+  }
+}
