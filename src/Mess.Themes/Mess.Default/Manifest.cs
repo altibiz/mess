f79d94ec1d3ddcd8142c2b107b93bc310d9ccb10
Hrvoje Juric <hrvoje@altibiz.com>
2023-02-03 15:09:17 +0100

    WIP

---
+++ src/Mess.Themes/Mess.Default/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+  Name = "Mess.Default",
+  Author = "Altibiz",
+  Website = "https://altibiz.com",
+  Version = "0.0.1",
+  Description = "Mess.Default",
+  Category = "Content Management"
+)]
