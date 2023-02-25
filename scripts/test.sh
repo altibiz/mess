621b74d0b5ab042e5ed09002b1b6c866f377f06a
Hrvoje Jurić <hrvoje@altibiz.com>
2023-02-25 19:41:38 +0100

    fix: test.sh and asset string

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,21 +5,21 @@ set -eo pipefail
 # export ASPNETCORE_ENVIRONMENT=Development
 # export DOTNET_ENVIRONMENT=Development
 
-printf "[Mess] Running 'docker-compose up -d'...\n"
 if [ ! "$CI" ]; then
+  printf "[Mess] Running 'docker-compose up --detach'...\n"
   docker-compose up --detach
 fi
 printf "\n"
 
 printf "[Mess] Tesing with 'dotnet'...\n"
 if [ "$1" ]; then
-  dotnet test \
-    --filter "FullyQualifiedName~$1"
+  dotnet test --filter "FullyQualifiedName~$1"
 else
   dotnet test
 fi
 printf "\n"
 
 if [ ! "$CI" ]; then
+  printf "[Mess] Running 'docker-compose down'...\n"
   docker-compose down
 fi
--- test/Mess.Modules/Mess.MeasurementDevice.Test/Assets/EgaugeAssets.cs
+++ test/Mess.Modules/Mess.MeasurementDevice.Test/Assets/EgaugeAssets.cs
@@ -5,5 +5,5 @@ namespace Mess.MeasurementDevice.Test.Assets;
 public static class EgaugeAssets
 {
   public static readonly string Measurement =
-    EmbeddedResources.GetStringEmbeddedResource("Assets.Egauge.xml");
+    EmbeddedResources.GetStringEmbeddedResource("assets.Egauge.xml");
 }
