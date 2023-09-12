4eeaf02d94138b7203fd70beb8aa6718c476de79
Hrvoje Jurić <hrvoje@altibiz.com>
2023-09-12 14:32:46 +0200

    WIP

---
+++ src/Mess.Modules/Mess.Ozds/Manifest.cs
@@ -0,0 +1,14 @@
+using OrchardCore.Modules.Manifest;
+using ManifestConstants = Mess.OrchardCore.ManifestConstants;
+
+[assembly: Module(
+  Id = "Mess.Ozds",
+  Name = "Ozds",
+  Description = "The Ozds project module.",
+  Author = ManifestConstants.Author,
+  Website = ManifestConstants.Website,
+  Version = ManifestConstants.Version,
+  Category = ManifestConstants.Category,
+  Tags = new string[] { ManifestConstants.MessTag },
+  Dependencies = new string[] { }
+)]
