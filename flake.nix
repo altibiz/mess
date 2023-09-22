544aa9f5cdf509db2c5cdba7aa64cca9bd2339fa
Hrvoje Jurić <hrvoje@altibiz.com>
2023-09-22 15:53:08 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -9,19 +9,27 @@
     utils.lib.simpleFlake {
       inherit self nixpkgs;
       name = "mess";
+      config = {
+        allowUnfree = true;
+      };
       overlay = (final: prev: {
         nodejs = prev.nodejs_20;
       });
       shell = { pkgs }:
         pkgs.mkShell {
           packages = with pkgs; [
+            git
+            helix
+            lazygit
             nodejs_20
             bun
             nodePackages.yarn
             nodePackages.typescript-language-server
+            nodePackages.vscode-langservers-extracted
             nodePackages.yaml-language-server
             dotnet-sdk_7
             omnisharp-roslyn
+            netcoredbg
             docker-client
             docker-compose
           ];
