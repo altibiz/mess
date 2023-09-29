b2db67c47faa4e163ef37f5328c9210313120185
Hrvoje Jurić <hrvoje@altibiz.com>
2023-09-29 15:34:39 +0200

    WIP

--- src/Mess.Abstractions/Mess.Billing.Abstractions/Receipts/Receipt.cs
+++ src/Mess.Abstractions/Mess.Billing.Abstractions/Receipts/Receipt.cs
@@ -5,6 +5,7 @@ namespace Mess.Billing.Abstractions.Receipts;
 public record Receipt(
   ContentItem[] Parties,
   ContentItem Issuer,
+  ContentItem Invoice,
   Guid Id,
   DateTime Issued,
   ReceiptSection[] Sections
