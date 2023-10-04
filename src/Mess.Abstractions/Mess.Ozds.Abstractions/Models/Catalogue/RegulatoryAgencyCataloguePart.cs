9524819c6f075b2c75cbac1cd25daab97e2062ae
Hrvoje Jurić <hrvoje@altibiz.com>
2023-10-04 17:23:37 +0200

    WIP

--- src/Mess.Abstractions/Mess.Ozds.Abstractions/Models/Catalogue/RegulatoryAgencyCataloguePart.cs
+++ src/Mess.Abstractions/Mess.Ozds.Abstractions/Models/Catalogue/RegulatoryAgencyCataloguePart.cs
@@ -1,7 +1,13 @@
+using OrchardCore.ContentFields.Fields;
 using OrchardCore.ContentManagement;
 
 namespace Mess.Ozds.Abstractions.Models;
 
 public class RegulatoryAgencyCataloguePart : ContentPart
 {
+  public NumericField RenewableEnergyFeePrice { get; set; } = default!;
+
+  public NumericField BusinessUsageFeePrice { get; set; } = default!;
+
+  public NumericField TaxRate { get; set; } = default!;
 }
