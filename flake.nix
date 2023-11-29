68c59e28dcae8c8acf3006f3d0a6f4dcf2f0a4ab
Hrvoje Jurić <hrvoje@altibiz.com>
2023-11-29 16:32:57 +0100

    WIP

--- flake.nix
+++ flake.nix
@@ -18,54 +18,73 @@
       });
       shell = { pkgs }:
         pkgs.mkShell {
-          packages = with pkgs; [
-            (pkgs.writeShellApplication {
-              name = "mess";
-              runtimeInputs = [ pkgs.nodePackages.yarn ];
-              text = ''
-                echo 'require("prettier")' |
-                  yarn node >/dev/null 2>&1 ||
-                  (printf "%s %s %s" \
-                    "\`prettier\` not found." \
-                    "Please make sure you run \`mess prepare\`" \
-                    "before running any other commands" && yarn)
+          packages = with pkgs; let
+            mess =
+              (pkgs.writeShellApplication {
+                name = "mess";
+                runtimeInputs = [ pkgs.nodePackages.yarn ];
+                text = ''
+                  echo 'require("prettier")' |
+                    yarn node >/dev/null 2>&1 ||
+                    (printf "%s %s %s" \
+                      "\`prettier\` not found." \
+                      "Please make sure you run \`mess prepare\`" \
+                      "before running any other commands" && yarn)
 
-                yarn scripts start "$@"
-              '';
-            })
+                  yarn scripts start "$@"
+                '';
+              });
+
             # TODO: https://github.com/dotnet/sdk/issues/30546
-            (pkgs.buildDotnetGlobalTool {
-              pname = "dotnet-csharpier";
-              nugetName = "CSharpier";
-              version = "0.25.0";
-              nugetSha256 = "sha256-7yRDI7vdLTXv0XuUHKUdsIJsqzmw3cidWjmbZ5g5Vvg=";
-              dotnet-sdk = pkgs.dotnetCorePackages.sdk_6_0;
-              dotnet-runtime = pkgs.dotnetCorePackages.sdk_6_0;
-              meta = with pkgs.lib; {
-                homepage = "https://github.com/belav/csharpier";
-                changelog = "https://github.com/belav/csharpier/blob/main/CHANGELOG.md";
-                license = licenses.mit;
-                platforms = platforms.linux;
-              };
-            })
+            csharpier =
+              (pkgs.buildDotnetGlobalTool {
+                pname = "dotnet-csharpier";
+                nugetName = "CSharpier";
+                version = "0.25.0";
+                nugetSha256 = "sha256-7yRDI7vdLTXv0XuUHKUdsIJsqzmw3cidWjmbZ5g5Vvg=";
+                dotnet-sdk = pkgs.dotnetCorePackages.sdk_6_0;
+                dotnet-runtime = pkgs.dotnetCorePackages.sdk_6_0;
+                meta = with pkgs.lib; {
+                  homepage = "https://github.com/belav/csharpier";
+                  changelog = "https://github.com/belav/csharpier/blob/main/CHANGELOG.md";
+                  license = licenses.mit;
+                  platforms = platforms.linux;
+                };
+              });
+
+            usql = pkgs.writeShellApplication {
+              name = "usql";
+              runtimeInputs = [ pkgs.usql ];
+              text = ''
+                usql pg://pidgeon:pidgeon@localhost/pidgeon?sslmode=disable "$@"
+              '';
+            };
+          in
+          [
+            # Nix
             nil
             nixpkgs-fmt
-            direnv
-            nix-direnv
-            git
-            helix
-            lazygit
+
+            # C#
+            dotnet-sdk
+            omnisharp-roslyn
+            netcoredbg
+            csharpier
+
+            # Web
             nodejs
-            bun
             nodePackages.yarn
             nodePackages.typescript-language-server
             nodePackages.vscode-langservers-extracted
             nodePackages.yaml-language-server
-            dotnet-sdk
-            omnisharp-roslyn
-            netcoredbg
-            docker-client
-            docker-compose
+
+            # Misc
+            usql
+            just
+            nodePackages.prettier
+            nodePackages.yaml-language-server
+            marksman
+            taplo
           ];
         };
     };
