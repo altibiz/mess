25402d13213ba557ed62b3b3ae5fe29a4572ab56
Hrvoje Jurić <hrvoje@altibiz.com>
2023-12-08 16:10:43 +0100

    WIP

---
+++ src/Mess.Modules/Mess.Ozds/Controllers/ClosedDistributionRepresentativeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Mess.Cms.Extensions.Microsoft;
+using OrchardCore.ContentManagement;
+using YesSql;
+
+namespace Mess.Ozds.Controllers;
+
+public class ClosedDistributionSystemRepresentativeController
+{
+  private readonly IContentManager _contentManager;
+  private readonly ISession _session;
+
+  public ClosedDistributionSystemRepresentativeController(
+    IContentManager contentManager,
+    ISession session
+  )
+  {
+    _contentManager = contentManager;
+    _session = session;
+  }
+
+  public async Task<IActionResult> Dashboard()
+  {
+    var orchardCoreUser = await this.GetAuthenticatedOrchardCoreUserAsync();
+
+    var legalEntityItem = await _session
+      .Query<ContentItem, UserPickerFieldIndex>()
+      .Where(index => index.ContentPart == "LegalEntityPart")
+      .Where(index => index.SelectedUserId == orchardCoreUser.UserId)
+      .FirstOrDefaultAsync();
+  }
+}
