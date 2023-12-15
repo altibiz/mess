fda3f6df10c0eb8d1d18f77fc859956b96eb9830
Hrvoje Jurić <hrvoje@altibiz.com>
2023-12-15 16:05:17 +0100

    WIP

--- src/Mess.Modules/Mess.Blazor/Controllers/AppController.cs
+++ src/Mess.Modules/Mess.Blazor/Controllers/AppController.cs
@@ -11,6 +11,6 @@ public class AppController : Controller
 {
   public async Task<IActionResult> Index()
   {
-    return await this.Component(typeof(App), null);
+    return View("/Areas/Mess.Blazor/Views/Shared/App.cshtml");
   }
 }
