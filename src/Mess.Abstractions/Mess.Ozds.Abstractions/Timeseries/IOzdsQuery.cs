e83f87b4116d8cc21b65fc7bb084049103bac666
Gabrijel Čmelić <gabrijel@altibiz.com>
2024-02-24 13:10:34 +0100

    WIP

--- src/Mess.Abstractions/Mess.Ozds.Abstractions/Timeseries/IOzdsQuery.cs
+++ src/Mess.Abstractions/Mess.Ozds.Abstractions/Timeseries/IOzdsQuery.cs
@@ -30,7 +30,44 @@ public partial interface IOzdsTimeseriesQuery
     DateTimeOffset toDate
   );
 
-  public Task<IReadOnlyList<AbbMeasurement>> GetBulkAbbMeasurementsAsync(
+  public Task<AbbMeasurement?> GetLastAbbMeasurementAsync(
+    string source
+  );
+
+  public AbbMeasurement? GetLastAbbMeasurement(
+    string source
+  );
+
+  public Task<
+    SchneiderMeasurement?
+  > GetLastSchneiderMeasurementAsync(
+    string source
+  );
+
+  public SchneiderMeasurement? GetLastSchneiderMeasurement(
+    string source
+  );
+
+  public Task<IReadOnlyList<AbbMeasurement>> GetBulkLastAbbMeasurementAsync(
+    IEnumerable<string> source
+  );
+
+  public IReadOnlyList<AbbMeasurement> GetBulkLastAbbMeasurement(
+    IEnumerable<string> source
+  );
+
+  public Task<
+    IReadOnlyList<SchneiderMeasurement>
+  > GetBulkLastSchneiderMeasurementAsync(
+    IEnumerable<string> source
+  );
+
+  public IReadOnlyList<SchneiderMeasurement> GetBulkLastSchneiderMeasurement(
+    IEnumerable<string> source
+  );
+
+
+  public Task<SchneiderMeasurement?> GetBulkAbbMeasurementsAsync(
     IEnumerable<string> sources,
     DateTimeOffset fromDate,
     DateTimeOffset toDate
--- src/Mess.Modules/Mess.Ozds/Timeseries/OzdsTimeseriesQuery.cs
+++ src/Mess.Modules/Mess.Ozds/Timeseries/OzdsTimeseriesQuery.cs
@@ -98,6 +98,185 @@ public partial class OzdsTimeseriesClient
     });
   }
 
+  public async Task<AbbMeasurement?> GetLastAbbMeasurementAsync(
+    string source
+  )
+  {
+    return await _services.WithTimeseriesDbContextAsync<
+      OzdsTimeseriesDbContext,
+      AbbMeasurement?
+    >(async context =>
+    {
+      return await context.AbbMeasurements
+        .Where(measurement => measurement.Source == source)
+        .OrderBy(measurement => measurement.Timestamp)
+        .Select(measurement => measurement.ToModel())
+        .LastOrDefaultAsync();
+    });
+  }
+
+  public AbbMeasurement? GetLastAbbMeasurement(
+    string source
+  )
+  {
+    return _services.WithTimeseriesDbContext<
+     OzdsTimeseriesDbContext,
+     AbbMeasurement?
+   >(context =>
+   {
+     return context.AbbMeasurements
+       .Where(measurement => measurement.Source == source)
+       .OrderBy(measurement => measurement.Timestamp)
+       .Select(measurement => measurement.ToModel())
+       .LastOrDefault();
+   });
+  }
+
+  public async Task<
+    SchneiderMeasurement?
+  > GetLastSchneiderMeasurementAsync(
+    string source
+  )
+  {
+    return await _services.WithTimeseriesDbContextAsync<
+      OzdsTimeseriesDbContext,
+      SchneiderMeasurement?
+    >(async context =>
+    {
+      return await context.SchneiderMeasurements
+        .Where(measurement => measurement.Source == source)
+        .OrderBy(measurement => measurement.Timestamp)
+        .Select(measurement => measurement.ToModel())
+        .LastOrDefaultAsync();
+    });
+  }
+
+  public SchneiderMeasurement? GetLastSchneiderMeasurement(
+    string source
+  )
+  {
+    return _services.WithTimeseriesDbContext<
+      OzdsTimeseriesDbContext,
+      SchneiderMeasurement?
+    >(context =>
+    {
+      return context.SchneiderMeasurements
+        .Where(measurement => measurement.Source == source)
+        .OrderBy(measurement => measurement.Timestamp)
+        .Select(measurement => measurement.ToModel())
+        .LastOrDefault();
+    });
+  }
+
+  public async Task<IReadOnlyList<AbbMeasurement>> GetBulkLastAbbMeasurementAsync(
+    IEnumerable<string> source
+  )
+  {
+    return await _services.WithTimeseriesDbContextAsync<
+      OzdsTimeseriesDbContext,
+      List<AbbMeasurement>
+    >(async context =>
+    {
+      var queries = source.Select(source => context.AbbMeasurements
+        .Where(measurement => measurement.Source == source)
+        .OrderBy(measurement => measurement.Timestamp)
+        .Select(measurement => measurement.ToModel())
+        .DeferredLastOrDefault()
+        .FutureValue()
+     );
+
+      var measurements = new List<AbbMeasurement>();
+      foreach (var query in queries)
+      {
+        measurements.Add(await query.ValueAsync());
+      }
+
+      return measurements;
+    });
+  }
+
+  public IReadOnlyList<AbbMeasurement> GetBulkLastAbbMeasurement(
+    IEnumerable<string> source
+  )
+  {
+    return _services.WithTimeseriesDbContext<
+     OzdsTimeseriesDbContext,
+     List<AbbMeasurement>
+   >(context =>
+   {
+     var queries = source.Select(source => context.AbbMeasurements
+        .Where(measurement => measurement.Source == source)
+        .OrderBy(measurement => measurement.Timestamp)
+        .Select(measurement => measurement.ToModel())
+        .DeferredLastOrDefault()
+        .FutureValue()
+     );
+
+     var measurements = new List<AbbMeasurement>();
+     foreach (var query in queries)
+     {
+       measurements.Add(query.Value);
+     }
+
+     return measurements;
+   });
+  }
+
+  public async Task<
+    IReadOnlyList<SchneiderMeasurement>
+  > GetBulkLastSchneiderMeasurementAsync(
+    IEnumerable<string> source
+  )
+  {
+    return await _services.WithTimeseriesDbContextAsync<
+      OzdsTimeseriesDbContext,
+      List<SchneiderMeasurement>
+    >(async context =>
+    {
+      var queries = source.Select(source => context.SchneiderMeasurements
+        .Where(measurement => measurement.Source == source)
+        .OrderBy(measurement => measurement.Timestamp)
+        .Select(measurement => measurement.ToModel())
+        .DeferredLastOrDefault()
+        .FutureValue()
+     );
+
+      var measurements = new List<SchneiderMeasurement>();
+      foreach (var query in queries)
+      {
+        measurements.Add(await query.ValueAsync());
+      }
+
+      return measurements;
+    });
+  }
+
+  public IReadOnlyList<SchneiderMeasurement> GetBulkLastSchneiderMeasurement(
+    IEnumerable<string> source
+  )
+  {
+    return _services.WithTimeseriesDbContext<
+      OzdsTimeseriesDbContext,
+      List<SchneiderMeasurement>
+    >(context =>
+    {
+      var queries = source.Select(source => context.SchneiderMeasurements
+        .Where(measurement => measurement.Source == source)
+        .OrderBy(measurement => measurement.Timestamp)
+        .Select(measurement => measurement.ToModel())
+        .DeferredLastOrDefault()
+        .FutureValue()
+     );
+
+      var measurements = new List<SchneiderMeasurement>();
+      foreach (var query in queries)
+      {
+        measurements.Add(query.Value);
+      }
+
+      return measurements;
+    });
+  }
   public IReadOnlyList<AbbMeasurement> GetBulkAbbMeasurements(
     IEnumerable<string> sources,
     DateTimeOffset fromDate,
